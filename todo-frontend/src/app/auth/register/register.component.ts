5132a7c3780233918581011e8b052e297896d569
John <An3439201@gmail.com>
2025-08-09 15:56:58 +0700

    Refactor code structure for improved readability and maintainability

--- todo-frontend/src/app/auth/register/register.component.ts
+++ todo-frontend/src/app/auth/register/register.component.ts
@@ -1,11 +1,43 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
-export class RegisterComponent {
 
+export class RegisterComponent {
+  registerForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.registerForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  get email() {
+    return this.registerForm.get('email')!;
+  }
+
+  get password() {
+    return this.registerForm.get('password')!;
+  }
+
+  onSubmit() {
+    if (this.registerForm.valid) {
+      console.log('Register data: ', this.registerForm.value);
+    }
+    else {
+      this.registerForm.markAllAsTouched();
+    }
+  }
+
+  onCancel() {
+    this.registerForm.reset();
+  }
 }
